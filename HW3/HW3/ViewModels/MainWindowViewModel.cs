bfc6de443c96664a0d85a2f8392e656bcfd8de64
cadechaplin <cade.chaplin@wsu.edu>
2024-02-07 16:25:12 -0800

    Crashing Issues 2/7

--- HW3/HW3/ViewModels/MainWindowViewModel.cs
+++ HW3/HW3/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,55 @@
 ﻿namespace HW3.ViewModels;
 
+using System.IO;
+using System.Reactive;
+using System.Reactive.Linq;
+using ReactiveUI;
+
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+
+    private string _currentText;
+    public string CurrentText
+    {
+        get => _currentText;
+        set => this.RaiseAndSetIfChanged(ref _currentText,  value);
+    }
+    
+
+    public MainWindowViewModel()
+    {
+// Create an interaction between the view model and the view for the file to be loaded:
+        AskForFileToLoad = new Interaction<Unit, string?>();
+// Similarly to load, there is a need to create an interaction for saving into a file:
+// TODO: Your code goes here.
+    }
+    public async void LoadFromFile()
+    {
+// Wait for the user to select the file to load from.
+        var filePath = await AskForFileToLoad.Handle(default);
+        if (filePath == null) return;
+// If the user selected a file, create the stream reader and load the text.
+        var textReader = new StreamReader(filePath);
+        LoadText(textReader);
+        textReader.Close();
+    }
+    private void LoadText(TextReader sr)
+    {
+            
+        CurrentText = sr.ReadToEnd();
+        //OnPropertyChanged(nameof(currentText));
+
+
+    }
+
+    
+
+    
+
+    public async void SaveToFile()
+    {
+// TODO: Implement this method.
+    }
+    public Interaction<Unit, string?> AskForFileToLoad { get; }
+
 }
--- HW3/HW3/Views/MainWindow.axaml.cs
+++ HW3/HW3/Views/MainWindow.axaml.cs
@@ -1,11 +1,42 @@
 using Avalonia.Controls;
 
 namespace HW3.Views;
-
-public partial class MainWindow : Window
+using System.Collections.Generic;
+using System.Reactive;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using HW3.ViewModels;
+using ReactiveUI;
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
+        this.WhenActivated(d =>
+            d(ViewModel!.AskForFileToLoad.RegisterHandler(DoOpenFile)));
+    }
+    private async Task DoOpenFile(InteractionContext<Unit, string?> interaction)
+    {
+        var fileDialog = new OpenFileDialog
+        {
+            AllowMultiple = false,
+        };
+        var txtFiler = new FileDialogFilter
+        {
+            Extensions = { "txt" },
+        };
+        var fileDialogFilters = new List<FileDialogFilter>
+        {
+            txtFiler,
+        };
+        fileDialog.Filters = fileDialogFilters;
+        var filePath = await fileDialog.ShowAsync(this);
+        interaction.SetOutput(filePath is { Length: 1 } ? filePath[0] : null);
+    }
+    private async Task DoSaveFile(InteractionContext<Unit, string?> interaction)
+    {
+// TODO: your code goes here.
     }
 }
