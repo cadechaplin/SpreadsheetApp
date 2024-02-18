751246584dd0517d7d7a61150b98279b0682a6ad
cadechaplin <cade.chaplin@wsu.edu>
2024-02-18 14:55:44 -0800

    started cell class

--- HW4/SpreadsheetEngine/Cell.cs
+++ HW4/SpreadsheetEngine/Cell.cs
@@ -2,4 +2,20 @@
 
 public abstract class Cell
 {
+    private readonly int _rowIndex;
+    private readonly int columnIndex;
+    
+    public string cellValue;
+
+    // Constructor to set the RowIndex value
+    public Cell(int setRowIndex, int setColumnIndex)
+    {
+        _rowIndex = setRowIndex;
+        columnIndex = setColumnIndex;
+
+    }
+
+    // Public read-only property to expose the RowIndex value
+    public int RowIndex => _rowIndex;
+    public int ColumnIndex => columnIndex;
 }
