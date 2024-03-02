2169d871b557315f9e01be989e8a95a16a12ab52
cadechaplin <cade.chaplin@wsu.edu>
2024-03-02 12:18:36 -0800

    added some test cases

--- HW4/ExpressionTreeTests/UnitTest1.cs
+++ HW4/ExpressionTreeTests/UnitTest1.cs
@@ -1,15 +1,37 @@
-namespace ExpressionTreeTests;
-
+namespace ExpressionTreeAppTests;
+using SpreadsheetEngine;
 public class Tests
 {
+   
     [SetUp]
     public void Setup()
     {
+        
+        
     }
 
     [Test]
-    public void Test1()
+    public void GeneralExpressionTest()
     {
-        Assert.Pass();
+        ExpressionTree exp = new ExpressionTree("3*3*3");
+        Assert.That(27, Is.EqualTo(exp.Evaluate()));
+    }
+
+    [Test]
+    [TestCase("3+5", ExpectedResult = 8.0)]
+    [TestCase("100/10*10", ExpectedResult = 100.0)]
+    [TestCase("100/(10*10)", ExpectedResult = 1.0)]
+    [TestCase("7-4+2", ExpectedResult = 5.0)]
+    [TestCase("10/(7-2)", ExpectedResult = 2.0)]
+    [TestCase("(12-2)/2", ExpectedResult = 5.0)]
+    [TestCase("((((((2+3)-(4+5))))))", ExpectedResult = -4.0)]
+    [TestCase("2*3+5", ExpectedResult = 11.0)]
+    [TestCase("2+3*5", ExpectedResult = 17.0)]
+    [TestCase("2 * 3 * 5", ExpectedResult = 17.0)]
+    [TestCase("5/0", ExpectedResult = double.PositiveInfinity)]
+    public double Test(string exp)
+    {
+        ExpressionTree expTree = new ExpressionTree(exp);
+        return expTree.Evaluate();
     }
 }
--- HW4/SpreadsheetEngine/ExpressionTree.cs
+++ HW4/SpreadsheetEngine/ExpressionTree.cs
@@ -2,5 +2,18 @@ namespace SpreadsheetEngine;
 
 public class ExpressionTree
 {
+    public ExpressionTree(string expression)
+    {
+        
+    }
+
+    public void SetVariable(string variableName, double variableValue)
+    {
     
+    }
+    
+    public double Evaluate()
+    {
+        return 0;
+    }
 }
