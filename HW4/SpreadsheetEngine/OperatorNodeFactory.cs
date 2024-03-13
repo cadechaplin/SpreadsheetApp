4dbb65350c966119e5488688c60e854dea2a6f0c
cadechaplin <cade.chaplin@wsu.edu>
2024-03-13 15:54:59 -0700

    created operatorNodeFactory class

---
+++ HW4/SpreadsheetEngine/OperatorNodeFactory.cs
@@ -0,0 +1,13 @@
+namespace SpreadsheetEngine;
+
+public class OperatorNodeFactory
+{
+    public OperatorNodeFactory()
+    {
+    }
+
+    internal OperatorNode createNode(string op)
+    {
+        throw new NotImplementedException("Not implemented");
+    }
+}
